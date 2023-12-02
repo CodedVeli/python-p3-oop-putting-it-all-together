3a5341b3e8fe6e6c7039ff0ea5a7c83d08eab49c
Eric Githaiga <ericgithaiga007@gmail.com>
2023-12-02 22:23:54 +0300

    solution

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,40 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+
+    def __init__(self, title, page_count):
+        if type(title) == str:
+            self._title = title
+        else:
+            print("Title must be a string")
+
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def get_title(self):
+        return self._title
     
-        
+    def set_title(self, title):
+        if type(title) == str:
+            self._title = title
+        else:
+            print("Title must be a string")
+
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    title = property(get_title, set_title)
+    page_count = property(get_page_count, set_page_count)
+
+# Path: lib/testing/book_test.py
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,56 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def __init__(self, brand, color, size):
+        if isinstance(brand, str):
+            self._brand = brand
+        else:
+            print("Brand must be a string")
+
+        if isinstance(color, str):
+            self._color = color
+        else:
+            print("Color must be a string")
+
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("Size must be an integer")
+
+    def get_brand(self):
+        return self._brand
+    
+    def set_brand(self, brand):
+        if isinstance(brand, str):
+            self._brand = brand
+        else:
+            print("Brand must be a string")
+
+    def get_color(self):
+        return self._color
+    
+    def set_color(self, color):
+        if isinstance(color, str):
+            self._color = color
+        else:
+            print("Color must be a string")
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("Size must be an integer")
+
+    def condition(self):
+        print("You've worn these shoes 0 times. They're brand new!")
+
+    def wear(self):
+        print("You've worn these shoes 1 time. They're still fresh!")
+
+    brand = property(get_brand, set_brand)
+    color = property(get_color, set_color)
+    size = property(get_size, set_size)
